1fa7c141e2e4ddceb7237c53d5d60c2a927af462
Samarth Rajput <ssamarth224@gmail.com>
2025-07-02 11:48:36 +0530

    fixed typ

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Acess-Control-Allow-Expose-Header', 'Content-length, Content-Range');
+    res.header('Access-Control-Allow-Expose-Header', 'Content-length, Content-Range');
     next();
 });
 
